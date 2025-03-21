d07e3f9b48f90a56fe0fc11e743a17f17b449533
Dilnaz <153282013+Dilnazochka@users.noreply.github.com>
2025-03-21 18:39:20 +0600

    add entities

---
+++ src/main/java/entities/Reservation.java
@@ -0,0 +1,26 @@
+package entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+}
