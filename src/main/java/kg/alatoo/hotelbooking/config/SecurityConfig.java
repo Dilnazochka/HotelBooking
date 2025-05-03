1d186d0b759dec59690aadfe0528ed80d68d6959
Dilnaz <153282013+Dilnazochka@users.noreply.github.com>
2025-05-03 09:05:43 +0600

    Added User entity and project configuration

---
+++ src/main/java/kg/alatoo/hotelbooking/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kg.alatoo.hotelbooking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // отключаем CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // разрешаем без авторизации
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // или .formLogin() / .oauth2Login() если хочешь GUI
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // для использования AuthenticationManager (нужно для логина)
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/kg/alatoo/hotelbooking/entities/User.java
@@ -0,0 +1,30 @@
+package kg.alatoo.hotelbooking.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private boolean emailVerified = false;
+
+    private String refreshToken;
+}
