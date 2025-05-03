7773f2959165b64202bf5ea4390074df8ae9f1d0
Dilnaz <153282013+Dilnazochka@users.noreply.github.com>
2025-05-03 10:46:51 +0600

    Integrate JWT-based authentication

---
+++ src/main/java/kg/alatoo/hotelbooking/security/JwtUtil.java
@@ -0,0 +1,49 @@
+package kg.alatoo.hotelbooking.security;
+
+import io.jsonwebtoken.*;
+import kg.alatoo.hotelbooking.entities.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String jwtSecret = "secret_key";
+    private final long jwtExpirationMs = 3600000;
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs * 24))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
